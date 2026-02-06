f160c5aafcfb0392f2baeabe83eec9c303d6d43b
Brett Halsey <Brett.m.halsey@gmail.com>
2026-02-05 19:13:50 -0500

    Ignore TS/ESLint errors for build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const withMDX = require('@next/mdx')()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = withMDX(nextConfig)
